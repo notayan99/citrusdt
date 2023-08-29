a2abdea26c202ed483754ad809477b0f1301f51a
notayan99 <ayan98542@gmail.com>
2023-08-29 09:56:34 +0000

    bringup

--- lineage_citrus.mk
+++ lineage_citrus.mk
@@ -9,15 +9,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from citrus device
 $(call inherit-product, device/xiaomi/citrus/device.mk)
 
-PRODUCT_NAME := aosp_citrus
+PRODUCT_NAME := lineage_citrus
 PRODUCT_DEVICE := citrus
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2010J19CG
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+RISING_MAINTAINER := Ayan
+RISING_OFFICIAL := true
+RISING_CHIPSET := SD662
+TARGET_ENABLE_BLUR := true
+TARGET_HAS_UDFPS := false
+TARGET_USE_PIXEL_FINGERPRINT := false
+TARGET_OPTOUT_GOOGLE_TELEPHONY := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
